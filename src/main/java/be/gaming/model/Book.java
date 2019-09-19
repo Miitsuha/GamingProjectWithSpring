075b046ee84afa9beb7b7b111a16ae1702ee8205
TheMinos <55382522+TheMinos@users.noreply.github.com>
2019-09-19 15:39:20 +0200

    Modification

--- src/main/java/be/gaming/model/Book.java
+++ src/main/java/be/gaming/model/Book.java
@@ -1,18 +1,12 @@
 package be.gaming.model;
 
-import javax.persistence.*;
-
-@Entity
 public class Book {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
     private long id;
 
-    @Column(nullable = false, unique = true)
     private String title;
 
-    @Column(nullable = false)
     private String author;
 
     public long getId() {
